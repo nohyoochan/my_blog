1c41a00b6e8a3c09fa8e4abf089991cfac24a06b
nohyoochan <ssense99@daum.net>
2020-07-08 18:44:52 +0900

    모작

---
+++ public/pf/sbs/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>maven</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="/pf/sbs/index.css">
+    <link rel="icon" href="https://nohyoochan.github.io/img1/blog/article/2020/logo1.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/pf/sbs/index.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
+</head>
+<body>
+<header id="header">
+    <div class="inner">
+        <a href="/pf/sbs/index.php" class="logo"></a>
+            <div class="menu">
+                <ul>
+                    <li class=""><a href="#">ABOUT</a></li>
+                    <li class=""><a href="#">PROJECT</a></li>
+                    <li class=""><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+    </div>
+</header>
+<div class="sec">
+                <div class="wrap">
+                    <h2><strong>IMAGINE,</strong>BE CREATIVE</h2>
+                    <p class="desc">메이븐디는 비즈니스 이해, UI/UX 설계 및 기획, 그리고 검증되고 고도화된 개발 로직
+                        <br>적용을 통해 최고의 비스를 제공 합니다. 시스템구축 후 운영 및 마케팅 전략까지,
+                        <br>E-BUSINESS의 모든 서비스를 제공할 수 있는 진정한 통합 온라인 전문기업입니다.
+                    </p>
+                </div>
+            </div>
+</body>
