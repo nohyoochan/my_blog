35353f5e425e48537f59348cf32bd120b13554e0
nohyoochan <ssense99@daum.net>
2020-07-09 18:11:00 +0900

    2차 작업

--- public/pf/sbs/index.php
+++ public/pf/sbs/index.php
@@ -34,4 +34,180 @@
                     </p>
                 </div>
             </div>
-</body>
+
+<div class="page2">
+    <div class="wrap">
+       <h2 class="h-first">메이븐디는 경험이 많은 숙련된 전문가를 의미 합니다.</h2> 
+       <p class="desc">메이븐디는 축적된 마케팅 경험과 전략적인 인사이트를 통해 디지털 마케팅의 기획에서 부터 제작 등 총제척인 솔루션을 구현합니다.</p>
+       <h2 class="h-second">디지털 크리에이티브 역량 강화</h2>
+       <p class="desc">메이븐디는 크리에이티브한 아이디어와 디자인 감각으로 브랜드가 전하고자 하는 메세지를 시각화하여 커뮤니케이션 합니다. <br> 메이븐디는 보다 편하고 즐겁게 디지털 플랫폼을 즐길 수 있도록 사용자 경험을 기반으로 한 인터페이스를 설계, 구현 합니다.</p>
+       <h2 class="h-three">최신 트랜드 기술 구현</h2>
+       <p class="desc">메이븐디는 웹/앱 플랫폼 디자인, 뉴미디어 플랫폼 디자인, 프로모션, 배너 디자인/호스팅 개발 운영/모니터링, 로그분석, 리포팅/고객 CS 관리 등 다양한 플랫폼 제작과 서비스를 제공하는 에이전시 입니다. 온라인 프로모션과 웹사이트 구속, 영상 제작을 기반으로 디지털 크리에이티브의 중심에서 웹 세상을 향해 내딛는 발자국마다 메이븐디가 함께합니다.</p>
+    </div>
+</div>
+
+<div class="page3">
+    <div class="wrap">
+        <h2>삼양 XEEN</h2>
+        <h3>SAMYANG XEEN │ 브랜드 사이트</h3>
+        <p class="desc2">-</p>
+        <p class="desc3">-</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page4">
+    <div class="wrap">
+    <h2>드래곤 시티</h2>
+        <h3>SEOUL DRAGON CITY │ 멤버쉽 사이트</h3>
+        <p class="desc2">-</p>
+        <p class="desc3">-</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+    <div class="page4-1"></div>
+</div>
+
+<div class="page5">
+    <div class="wrap">
+    <h2>드롱기</h2>
+        <h3>DeLonghi │ VIP 멤버쉽 사이트</h3>
+        <p class=info>사용자 편의성 중대를 목적으로 기존 홈페이지의 크로스브라우징 및 모바일 환경에서 오류가 많던 낙후된 홈페이지의 개편을 통해 전반적인 사용성 스트레스를 줄이고 기존에 없던 장바구니 기능추가를 통해 멤버쉽 혜택 상품들을 편리하게 결재할 수 있도록 전반적인 UI/UX도 업그레이드 했습니다.</p>
+        <p class="info">로그인 전, 후 화면을 사용자 맞춤 정보로 제공하여 멤버십의 유효기간등 만료일전에 인지 하도록 하며 멤버십의 프리미엄을 더욱더 높여주고 있습니다.</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page6">
+    <div class="wrap">
+    <h2>국기원</h2>
+        <h3>KUKKIWON │ 홈페이지</h3>
+        <p class="desc2">-</p>
+        <p class="desc3">-</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page7">
+    <div class="wrap">
+    <h2>페이레터</h2>
+        <h3>PAYLETTER │ 브랜드 사이트</h3>
+        <p class="desc2">-</p>
+        <p class="desc3">-</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page8">
+    <div class="wrap">
+    <h2>하이트 진로</h2>
+        <h3>HITE JINRO │ 그룹웨어</h3>
+        <p class="desc2">-</p>
+        <p class="desc3">-</p>
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page9">
+    <div class="wrap">
+        <h2>Home & service Store</h2>
+        <h3>SK홈앤서비스 │ 브랜드 사이트</h3>
+        <p class=info>홈앤서비스 주식회사는 홈서비스의 혁신을 통해 고객에게 Home(가정)과 관련된 차별화된 Total Service를 제공하는 것을 목표로 하는 마케팅&서비스 기업입니다.</p>
+        <p class=info>SK 브로드밴드 인터넷 가입에 대한 전체적인 서비스를 제공하기 위해 제작된 사이트로 사용자의 인터넷 가입을 유도하는 페이지를 가장 주력으로구축하였습니다.</p>
+
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page10">
+    <div class="wrap">
+    <h2>삼양옵틱스</h2>
+        <h3>SAMYANG │ 브랜드 사이트</h3>
+        <p class=info>삼양옵틱스는 국내 유일의 카메라 렌즈 제작업체입니다.</p>
+        <p class=info>카메라 렌즈는 제품에 대한 사용자의 관여도가 높은 분야로 기존 사이트 보다 제품에 관한 디테일한 정보 및 제품 리뷰, 메뉴얼을 제공하고 실제 렌즈로 촬영한 고퀄리티 샘플 사진을 메인 배너로 적극 활용하여 기존 제품 사진 위주로 노출하는 방식을 탈피하여 사이트 자체의 볼거리를 강조하고 있습니다.</p>
+
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page11">
+    <div class="wrap">
+    <h2>봄여행주간 캠페인</h2>
+        <h3>한국관광공사 │ 마이크로 사이트</h3>
+        <p class=info>여행주간 기간 동안 전국의 지자체 및 관광업계가 협력하여 다양하고 특별한 여행 프로그램, 혜택 등을 제공하고 있습니다.</p>
+        <p class=info>검색 기능을 통해 이용자가 찾기 쉽게 정보를 제공하고, 마이페이지의 소셜 로그인 기능을 통해 이용자 스스로가 사이트내 각종 유익한 여행정보들을 선택하여 스크랩 하거나 자신의 SNS에 공유할 수 있도록 구현되어 있습니다.</p>
+
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page12">
+    <div class="wrap">
+    <h2>컬러어떰 캠페인</h2>
+        <h3>아모레퍼시픽 │ 캠페인 프로모션</h3>
+        <p class=info>올 가을 새롭게 탄생한 MLBB 립 컬러인 으깬장미를 홍보하기 위한 컬러어떰 캠페인 제작.</p>
+        <p class=info>컬러어떰 (Color Autumn)캠페인 사이트에서 제품의 정보와 이벤트참여 구매까지, 접근성을 높이고 사용자들이 쉽게 내용들을 볼 수 있도록 페이지 이동없이 원페이지로 구성했다.</p>
+
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page13">
+    <div class="wrap">
+    <h2>유플러스 홈서비스</h2>
+        <h3>LG U+ │ 브랜드 체험 사이트</h3>
+        <p class=info>유플러스 홈서비스 사이트는 U+tv 아이들나라 / loT@Home / U+우리집AI / U+웹진 우리집 서비스를 소개하고 고객들이 직접 체험한 후기를 공유하는 사이트 입니다.</p>
+ 
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page14">
+    <div class="wrap">
+    <h2>르노삼성자동차 Facebook 캔버스</h2>
+        <h3>르노삼성자동차 │ Facebook 캔버스</h3>
+        <p class=info>르노삼성자동차 페이스북은 2018년 10월 현재까지 참여자들의 활발한 참여를 이끌어 게시물별 7만-12만 넘는 높은 도달수를 기록하고 있으며, 월 1500명 가량 팬수 증가로 현재, 11만 이상의 팬수를 확보하였다.
+        </p>
+ 
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page15">
+    <div class="wrap">
+    <h2>유한킴벌리 좋은느낌 '솜솜'</h2>
+        <h3>유한킴벌리 │ 아이디어 공모전</h3>
+        <p class=info>유한킴벌리는 좋은느낌 '솜솜'이 젊은 세대를 대상으로 선보인 새로운 제품인 만큼, 제품캐릭터인 '소미'의 이야기와 그림을 대학생들의 의견을 반영해 함께 브랜드 이미지를 만들어 나가는 의도로 공모전을 개최하게 되었다.
+        </p>
+ 
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page16">
+    <div class="wrap">
+    <h2>도로교통공단 민원서비스 홈페이지</h2>
+        <h3>도로교통공단 │ 프로토타입 제작</h3>
+        <p class=info>도로교통공단의 민원 서비스 홈페이지 리뉴얼 진행중이다.
+        <br>
+        두 스타일의 프로토타입을 제작, 각각 PC Web메인화면과 모바일웹 화면
+        <br>
+        UI 디자인을 제작했다.
+        </p>
+ 
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+
+<div class="page17">
+    <div class="wrap">
+    <h2>웹케시</h2>
+        <h3>WEBCASH │ 브랜드 사이트</h3>
+        <p class=info>국내 유일 B2B 판테크 전문회사 웹케시 홈페이지를 제작.
+        <br>
+        모든 금융기관과 기업 ERP를 연결하여 국내 하나뿐인 기업금융접점 플랫폼
+        </p>
+ 
+        <a href="#" class="more">+ Details</a>
+    </div>
+</div>
+</body> 
