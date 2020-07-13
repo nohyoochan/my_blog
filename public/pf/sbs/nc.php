44513d7de2c185d8dabb5cf26410dbda2748fe76
nohyoochan <ssense99@daum.net>
2020-07-13 18:43:12 +0900

    수정보완

--- public/pf/sbs/nc.php
+++ public/pf/sbs/nc.php
@@ -20,11 +20,84 @@
             <div class="logo-bar">
                 <h1 class="logo">
                     <a href="#">
-                        <img src="https://nohyoochan.github.io/img1/pf/nc/logo.png" style="width; 200px;" alt="">
+                        <img src="https://nohyoochan.github.io/img1/pf/nc/logo.png" style="width:200px" alt="">
                     </a>
                 </h1>
             </div>
+    <!-- 메뉴바 -->
+        <div class="menu-bar">
+            <div class="menu-box">
+                <nav class="menu">
+                    <ul>
+                        <li>
+                            <a href="#">WE</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">News</a></li>
+                                    <li>
+                                        <a href="#">Brand Works</a></li>
+                                    <li>
+                                        <a href="#">The Originality</a></li>
+                                    <li>
+                                        <a href="#">Creator Crew</a></li>
+                                </ul>
+                            </li>
+                        <li>
+                            <a href="#">PLAY</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">리니지</a></li>
+                                    <li>
+                                        <a href="#">리니지2</a></li>
+                                    <li>
+                                        <a href="#">리니지M</a></li>
+                                    <li>
+                                        <a href="#">리니지2M</a></li>
+                                    <li>
+                                        <a href="#">아이온</a></li>
+                                    <li>
+                                        <a href="#">블레이드 & 소울</a></li>
+                                    <li>
+                                        <a href="#">프로야구 H2</a></li>
+                                    <li>
+                                        <a href="#">파이널 블레이드</a></li>
+                                    <li>
+                                        <a href="#">NC 다이노스</a></li>
+                                    <li>
+                                        <a href="#">버프툰</a></li>
+                                    <li>
+                                        <a href="#">스푼즈</a></li>
+                                    <li>
+                                        <a href="#">투턱곰</a></li>
+                                </ul>
+                            </li>
+                        <li>
+                            <a href="#">R&D</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">AI</a></li>
+                                    <li>
+                                        <a href="#">데이터 사이언스</a></li>
+                                    <li>
+                                        <a href="#">비주얼 아트</a></li>
+                                    <li>
+                                        <a href="#">사운드 디자인</a></li>
+                                </ul>
+                            </li>
+                        <li>
+                            <a href="#">VIEW</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">칼럼</a></li>
+                                    <li>
+                                        <a href="#">웹툰</a></li>
+                                </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-    </header>
+    </div>
+</header>
 </div>
 </body>
