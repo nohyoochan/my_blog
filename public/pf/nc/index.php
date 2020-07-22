3d2b02aae9b8c11c62e3318e4bccde137420be83
nohyoochan <ssense99@daum.net>
2020-07-22 18:37:31 +0900

    07-22 작업

--- public/pf/nc/index.php
+++ public/pf/nc/index.php
@@ -25,94 +25,184 @@
                 </h1>
             </div>
         </div>
-    <!-- 메뉴바 -->
         <div class="bottom_headbx">
             <div class="max_container">
-                <nav class="main-navigation">
-                    <div class="menu-main_menu-container">
-                    <ul class="main-menu">
-                        <li>
-                            <a href="#">WE</a>
-                                <ul class="sub-menu">
-                                    <li>
-                                        <a href="#">News</a></li>
-                                    <li>
-                                        <a href="#">Brand Works</a></li>
-                                    <li>
-                                        <a href="#">The Originality</a></li>
-                                    <li>
-                                        <a href="#">Creator Crew</a></li>
-                                </ul>
+                <!-- 네비 -->
+				 <nav id="site-navigation" class="main-navigation" aria-label="Top Menu">
+					<div class="menu-main_menu-container" style="height:56px;">
+                        <ul class="main-menu" style="height:56px;">
+                            <li class="menu-item">
+                                <a href="">WE</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item">
+                                            <a href="">News</a></li>
+                                        <li class="menu-item">
+                                            <a href="">Brand Works</a></li>
+                                        <li class="menu-item">
+                                            <a href="">The Originality</a></li>
+                                        <li class="menu-item">
+                                            <a href="">Creator Crew</a></li>
+                                    </ul>
                             </li>
-                        <li>
-                            <a href="#">PLAY</a>
-                                <ul class="sub-menu">
-                                    <li>
-                                        <a href="#">리니지</a></li>
-                                    <li>
-                                        <a href="#">리니지2</a></li>
-                                    <li>
-                                        <a href="#">리니지M</a></li>
-                                    <li>
-                                        <a href="#">리니지2M</a></li>
-                                    <li>
-                                        <a href="#">아이온</a></li>
-                                    <li>
-                                        <a href="#">블레이드 & 소울</a></li>
-                                    <li>
-                                        <a href="#">프로야구 H2</a></li>
-                                    <li>
-                                        <a href="#">파이널 블레이드</a></li>
-                                    <li>
-                                        <a href="#">NC 다이노스</a></li>
-                                    <li>
-                                        <a href="#">버프툰</a></li>
-                                    <li>
-                                        <a href="#">스푼즈</a></li>
-                                    <li>
-                                        <a href="#">투턱곰</a></li>
-                                </ul>
+
+                            <li class="menu-item">
+                                <a href="">PLAY</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item">
+                                            <a href="">리니지</a></li>
+                                        <li class="menu-item">
+                                                <a href="">리니지2</a></li>
+                                        <li class="menu-item">
+                                            <a href="">리니지M</a></li>
+                                        <li class="menu-item">
+                                            <a href="">리니지2M</a></li>
+                                        <li class="menu-item">
+                                            <a href="">아이온</a></li>
+                                        <li class="menu-item">
+                                            <a href="">블레이드 &amp; 소울</a></li>
+                                        <li class="menu-item">
+                                            <a href="">프로야구 H2</a></li>
+                                        <li class="menu-item">
+                                            <a href="">파이널 블레이드</a></li>
+                                        <li class="menu-item">
+                                            <a href="">NC 다이노스</a></li>
+                                        <li class="menu-item">
+                                            <a href="">버프툰</a></li>
+                                        <li class="menu-item">
+                                            <a href="">스푼즈</a></li>
+                                        <li class="menu-item">
+                                            <a href="">투턱곰</a></li>
+                                        </ul>
+                                </li>
+
+                            <li class="menu-item">
+                                <a href="">R&amp;D</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item">
+                                            <a href="">AI</a></li>
+                                        <li class="menu-item">
+                                            <a href="">데이터 사이언스</a></li>
+                                        <li class="menu-item">
+                                            <a href="">비주얼 아트</a></li>
+                                        <li class="menu-item">
+                                            <a href="">사운드 디자인</a></li>
+                                    </ul>
                             </li>
-                        <li>
-                            <a href="#">R&D</a>
-                                <ul class="sub-menu">
-                                    <li>
-                                        <a href="#">AI</a></li>
-                                    <li>
-                                        <a href="#">데이터 사이언스</a></li>
-                                    <li>
-                                        <a href="#">비주얼 아트</a></li>
-                                    <li>
-                                        <a href="#">사운드 디자인</a></li>
-                                </ul>
+
+                            <li class="menu-item">
+                                <a href="">VIEW</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item">
+                                            <a href="">칼럼</a></li>
+                                        <li class="menu-item">
+                                            <a href="">웹툰</a></li>
+                                    </ul>
                             </li>
-                        <li>
-                            <a href="#">VIEW</a>
-                                <ul class="sub-menu">
-                                    <li>
-                                        <a href="#">칼럼</a></li>
-                                    <li>
-                                        <a href="#">웹툰</a></li>
-                                </ul>
-                        </li>
-                    </ul>
+                        </ul>
+                    </div>                   
                 </nav>
+                <!-- .네비 -->
+                
+                <!-- 서치박스 -->
+                <div class="header_searchbx">
+                    <a href="#;" class="head_search_btn">검색하기</a>
+                    <div class="head_search_inputbx">
+                        <div class="textbx"><input type="text" class="head_search_text" name="head_search_text" value="" placeholder="원하는 검색어를 입력하세요.">
+                        </div>
+                        <input type="submit" class="head_search_submit" value="검색하기">
+                    </div>
                 </div>
+                <!-- .서치박스 -->
             </div>
         </div>
-    </div>
-</header>
 
 <div class="content">
     <div class="home-banner">
         <div class="h-banner">
-            <a href="#">
+            <a href="#" class="itembx">
                 <div class="bg">
                     <img src="https://nohyoochan.github.io/img1/pf/nc/page1.jpg" alt="">
+                </div>
+                <div class="cont">
+                    <div class="sub-title">Language AI Lab Executive Director 이연수</div>
+                    <h3 class="title">끝이라고 생각했을 때가 시작이다</h3>
                 </div>
             </a>
         </div>
     </div>
 </div>
+</div>
+
+
+<div class="main-content">
+    <div class="max_container">
+        <!-- 검색바 -->
+        <div class="main-search">
+            <div class="main-search-item">
+                <input type="text" class="main_search_text" name="main_search_text" placeholder="원하는 검색어를 입력하세요.">
+                <input type="submit" class="main_search_submit" value="검색하기">
+            </div>
+        </div>
+        <div class="search-tag">
+            <dl>
+                <dt>추천 해시태그</dt>
+                <dd>
+                <div class="bs-tag-bx">
+                <div class="tag-container">
+                    <a href="#" class="tag-item">#Brand Works</a>
+                    <a href="#" class="tag-item">#게임원화</a>
+                    <a href="#" class="tag-item">#엔씨라이프</a>
+                    <a href="#" class="tag-item">#AI</a>
+                    <a href="#" class="tag-item">#데이터 사이언스</a>
+                </div>
+                </div>
+                </dd>
+            </dl>
+        </div>
+    </div>
+</div>
+<!-- 검색바 -->
+
+<!-- 추천시리즈 -->
+<h2 class="title-2 center_sc">추천 시리즈</h2>
+<div class="series_lsbx">
+                <div class="thum_ro"><img src="https://imgblog.ncsoft.net/wp-content/uploads/2019/09/The-Originality.jpg" alt=""></div>
+                <div class="ls_ro">
+                    <ul>
+						                            <li class="active"><!-- 1번째 체크된상태로 시작 -->
+                                <div class="series_ps_item">
+                                    <a href="#;" class="itembx" data-cate="4790" data-image="https://imgblog.ncsoft.net/wp-content/uploads/2019/09/The-Originality.jpg" data-posts="">
+                                        <h3 class="m_sz_title">#The Originality</h3>
+                                        <p class="sub_text">엔씨 퀄리티의 시작</p>
+                                    </a>
+                                </div>
+                            </li>
+							                            <li><!-- 1번째 체크된상태로 시작 -->
+                                <div class="series_ps_item">
+                                    <a href="#;" class="itembx" data-cate="" data-image="https://imgblog.ncsoft.net/wp-content/uploads/2020/05/Brand-Works_Thumb2_j.jpg" data-posts="68356,67311,66199,66461,66656,67535,67829,69926,70126">
+                                        <h3 class="m_sz_title">#Brand Works</h3>
+                                        <p class="sub_text">즐거움으로 연결된 새로운 세상</p>
+                                    </a>
+                                </div>
+                            </li>
+							                            <li><!-- 1번째 체크된상태로 시작 -->
+                                <div class="series_ps_item">
+                                    <a href="#;" class="itembx" data-cate="" data-image="https://imgblog.ncsoft.net/wp-content/uploads/2020/02/the-game-art-2.jpg" data-posts="70750,70241,65124,61974,62877,60517,56786,64803,50293,66053,66573,67611,69407">
+                                        <h3 class="m_sz_title">#The Game Art</h3>
+                                        <p class="sub_text">엔씨의 게임 그래픽 전시</p>
+                                    </a>
+                                </div>
+                            </li>
+							                            <li><!-- 1번째 체크된상태로 시작 -->
+                                <div class="series_ps_item">
+                                    <a href="#;" class="itembx" data-cate="6537" data-image="https://imgblog.ncsoft.net/wp-content/uploads/2020/07/quest.jpg" data-posts="">
+                                        <h3 class="m_sz_title">#인생은 퀘스트</h3>
+                                        <p class="sub_text">게임 같은 인생 이야기</p>
+                                    </a>
+                                </div>
+                            </li>
+							                    </ul>
+                </div>
+            </div>
 </div>
 </body>
