703a8d2993d7ed6af4533e9b186a539221422a2b
nohyoochan <ssense99@daum.net>
2020-07-31 18:39:22 +0900

    07-31

--- public/pf/pizza/index.php
+++ public/pf/pizza/index.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>도미노피자 - 당신의 인생에 완벽한</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.slick/1.5.5/slick.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.slick/1.5.5/slick-theme.css">
+    <link rel="stylesheet" href="https://kenwheeler.github.io/slick/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
@@ -16,6 +20,8 @@
     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src="../package/swiper-bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.slick/1.5.5/slick.min.js"></script>
     <script src="/pf/pizza/index.js"></script>
 </head>
 <body>
@@ -164,21 +170,101 @@
                 <div class="swiper-button-next"></div>
                 <div class="swiper-button-prev"></div>
                 </div>
-
+                <!-- 주문화면 -->
                 <article class="delivery">
                     <div class="inner-box">
-                        <div class="item-wrap v2">
+                        <div class="item-wrap2">
                             <div class="item">
                                 <a href="#">
                                     <i class="ico-delivery"></i>
-                                    <span class="text">배달주문</span>
+                                    <span class="text">배달 주문</span>
                                     <span class="arrow"></span>
                                 </a>
                                 <p class="delivery-text">
                                     등급별 최대 30%
                                 </p>
                             </div>
-                            <div class="item"></div>
+                            <div class="item">
+                                <a href="#">
+                                    <i class="ico-takeout"></i>
+                                    <span class="text">포장 주문</span>
+                                    <span class="arrow"></span>
+                                </a>
+                                <p class="wrap-box">등급별 최대 40%</p>
+                            </div>
+                        </div>
+                        <div class="item-wrap">
+                            <div class="item">
+                                <a href="#">
+                                    <i class="ico-chat"></i>
+                                    <span class="text2">도미챗 주문</span>
+                                    <span class="arrow2"></span>
+                                </a>
+                                <p class="domichat-box">빠르고 간편한 채팅 주문 서비스</p>
+                            </div>
+                            <div class="item">
+                                <a href="#">
+                                    <i class="ico-quick"></i>
+                                    <span class="text2">퀵오더</span>
+                                    <span class="arrow2"></span>
+                                </a>
+                                <p>자주 이용하는 메뉴를<br>
+                                더 빠르게 주문해보세요!
+                                </p>
+                            </div>
+                            <div class="item">
+                                <a href="#">
+                                    <i class="ico-driving2"></i>
+                                    <span class="text2">드라이빙 픽업 주문</span>
+                                    <span class="arrow2"></span>
+                                </a>
+                                <p>고객님의 차로 직접<br>
+                                피자를 가져다 드립니다~
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <!-- 혜택 화면 -->
+                <article class="grade">
+                    <div class="inner-box">
+                        <div class="grade-info">
+                            <div class="login-before">
+                                <div class="title-wrap">
+                                    <div class="title-type2">
+                                        도미노피자의<br>
+                                        <Strong>특별한 매니아 혜택</Strong><br>
+                                        누려보세요!
+                                    </div>
+                                </div>
+                                <div class="btn-wrap">
+                                    <a href="#" class="btn-type5">로그인</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grade-slider">
+                            <div class="content">
+                            <div class="js-tp-slider-filter">
+                                <div class="js-slider"><img src="https://nohyoochan.github.io/img1/pf/pizza/main_card1.png" alt=""></div>
+                                 <div class="js-slider"><img src="https://nohyoochan.github.io/img1/pf/pizza/main_card2.gif" alt=""></div>
+                                 <div class="js-slider"><img src="https://nohyoochan.github.io/img1/pf/pizza/main_card3.png" alt=""></div>
+                                <div class="js-slider"><img src="https://nohyoochan.github.io/img1/pf/pizza/main_card5.png" alt=""></div>
+                            </div>
+                            </div>
+                            </section>
+                        </div>
+                    </div>
+                </article>
+                <!-- 배너 -->
+                <article class="banner">
+                    <div class="inner-box">
+                        <div class="banner-box">
+                            <a href="#">
+                                <img src="https://nohyoochan.github.io/img1/pf/pizza/cont2.jpg" alt="">
+                            </a>
+                            <a href="#">
+                                <img src="https://nohyoochan.github.io/img1/pf/pizza/cont1.jpg" alt="">
+                            </a>
                         </div>
                     </div>
                 </article>
