1aa6b3974963a8b37ddb96517f82881dbd0401df
nohyoochan <ssense99@daum.net>
2020-08-03 16:48:18 +0900

    pizza 작업 90%

--- public/pf/pizza/index.php
+++ public/pf/pizza/index.php
@@ -256,20 +256,165 @@
                     </div>
                 </article>
                 <!-- 배너 -->
-                <article class="banner">
+                <article class="banner-bar">
                     <div class="inner-box">
                         <div class="banner-box">
-                            <a href="#">
-                                <img src="https://nohyoochan.github.io/img1/pf/pizza/cont2.jpg" alt="">
-                            </a>
-                            <a href="#">
-                                <img src="https://nohyoochan.github.io/img1/pf/pizza/cont1.jpg" alt="">
-                            </a>
+                           <div class="poster">
+                                <div class="poster-bar">
+                                    <div class="poster-item">
+                                        <a href="#">
+                                            <img src="https://nohyoochan.github.io/img1/pf/pizza/cont2.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="poster-item">
+                                        <a href="#">
+                                            <img src="https://nohyoochan.github.io/img1/pf/pizza/cont1.jpg" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                           </div>
                         </div>
                     </div>
                 </article>
             </div>
         </section>
     </div>
+    <footer class="footer">
+        <div class="footer-bar">
+            <div class="inner-box">
+                <div class="footer-order">
+                    <i class="ico-logo2"></i>
+                    <span class="tel">1577-3082</span>
+                </div>
+                <ul class="footer-contact">
+                    <li>
+                        <a href="#">이용약관</a>
+                    </li>
+                    <li class="on">
+                        <a href="#">개인정보처리방침</a>
+                    </li>
+                    <li>
+                        <a href="#">고객센터</a>
+                    </li>
+                    <li>
+                        <a href="#">인재채용</a>
+                    </li>
+                    <li>
+                        <a href="#">가맹점모집</a>
+                    </li>
+                    <li>
+                        <a href="#">단체주문</a>
+                    </li>
+                </ul>
+                <div class="footer-info">
+                    <p>06229 서울특별시 강남구 언주로 315 청오디피케이㈜｜대표이사 : 오광현</p>
+                    <p>사업자 등록번호: 220-81-03371｜통신판매업신고: 강남 5064호｜개인정보 보호책임자 : 이명윤</p>
+                    <p>문의 : webmaster@dominos.co.kr</p>
+                    <p>Copyright ⓒ Domino’s Pizza. All rights reserved.</p>
+                    <p class="notice">
+                    청오디피케이㈜의 사전 서면동의 없이 도미노피자 사이트(PC, 모바일, 앱)의 일체의 정보, 콘텐츠 및 UI 등을 상업적 목적으로 전재, 전송, 스크래핑 등 무단 사용할 수 없습니다.
+                    </p>
+                </div>
+                <div class="footer-cont">
+                    <div class="select-type2 language">
+		                <select id="select-type2">
+							<option value="/main?locale=ko">KOR</option>
+							<option value="/main?locale=en">ENG</option>
+						</select>
+                    </div>
+                    <dl class="app-box">
+                        <dt>DOWNLOAD APP</dt>
+                        <dd>
+                            <a href="#">
+                                <i class="ico-ios"></i>
+                            </a>
+                        </dd>
+                        <dd>
+                            <a href="#">
+                                <i class="ico-android"></i>
+                            </a>
+                        </dd>
+                    </dl>
+                    <div class="sns-box">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="ico-blog"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="ico-insta"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="ico-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="ico-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="ico-twitter"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="award-bar">
+            <div class="inner-box">
+                <ul>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards.png" alt="">
+                        <p>식품안전
+                            <br>
+                            경영시스템 인증
+                        </p>
+                    </li>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards2.png" alt="">
+                        <p>지식경제부
+                            <br>
+                            우수디자인 선정
+                        </p>
+                    </li>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards3.png" alt="">
+                        <p>고객이 가장 추천하는 기업
+                            <br>
+                            피자전문점 부문 7년 연속 1위
+                        </p>
+                    </li>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards4.png" alt="">
+                        <p>2019년 한국산업 고객만족도
+                            <br>
+                            피자전문점 부문 5년 연속 1위
+                        </p>
+                    </li>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards5.png" alt="">
+                        <p>2019 프리미엄브랜드지수
+                            <br>
+                            피자전문점 부문 4년 연속 1위 수상
+                        </p>
+                    </li>
+                    <li>
+                        <img src="https://nohyoochan.github.io/img1/pf/pizza/list_awards6.png" alt="">
+                        <p>대학생 1000명이 선택한
+                            <br>
+                            2019 올해의 핫 브랜드 캠퍼스 잡앤조이 선정
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </div>
 </body>
