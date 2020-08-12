9dbe5928b112f78d58745bdc7280d4545fe1e6d5
nohyoochan <ssense99@daum.net>
2020-08-12 18:47:50 +0900

    08-12

---
+++ public/pf/Rolex/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROLEX 공식 판매점 - 대한민국</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/pf/Rolex/index.css">
+    <link rel="icon" href="https://nohyoochan.github.io/img1/pf/pf1/logo2.png">
+    <!-- 구글 폰트 불러오기 -->
+    <!-- rotobo(400/700/900), notosanskr(400/600/900) -->
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+    <!-- 폰트어썸 불러오기 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <!-- 제이쿼리 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/pf/Rolex/index.js"></script>
+</head>
+<body>
+<div class="top-bar">
+    <nav class="menu-box">
+        <a href="#" class="logo">
+            <img src="https://nohyoochan.github.io/img1/pf/pf1/logo.png" alt="" style="width:130px; height:70px;">
+            <img src="https://nohyoochan.github.io/img1/pf/pf1/logo2.png" alt="" style="width:100px; height:70px;">
+        </a>
+        <ul>
+            <div class="inner flex flex-jc-c"> 
+                <li>
+                <a href="#">Collection</a>
+                <ul class="flex">
+                    <li><a href="#">
+                        <img src="" alt=""></a></li> 
+                    <li><a href="#">
+                        <img src="" alt="">
+                    </a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Rolex And Partners</a>
+                <ul>  
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Service</a>
+                <ul>
+                    <li><a href="#"> </a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </li>
+        </div>
+        </ul>
+    </nav>
+</div>
+</body>
