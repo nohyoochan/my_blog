2048345decd0736698483ad04755ecee481c0910
ssense99@daum.net <ssense99@daum.net>
2020-08-18 18:43:58 +0900

    08-18

--- public/pf/Rolex/index.js
+++ public/pf/Rolex/index.js
@@ -1,15 +1,51 @@
 $(function (){
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        centeredSlides: true,
-        spaceBetween: 30,
-        grabCursor: true,
-        pagination: {
-        clickable: true,
-        },
+        slidesPerView: 3,
+        spaceBetween: 1,
+        freeMode: true,
         scrollbar: {
             el: '.swiper-scrollbar',
             draggable: true,
           },
-    })
+    });
+});
+
+$(function TabBox__init(){
+    $('[data-tab-head-item-name]').click(function() {
+        var $this = $(this);
+        var tabName = $this.attr('data-tab-name');
+        var itemName = $this.attr('data-tab-head-item-name');
+        // [for]
+        // 모든 것을 숨기고
+        $('[data-tab-name="' + tabName + '"]').removeClass('active');
+        
+        $('[data-tab-name="' + tabName + '"][data-tab-head-item-name="' + itemName + '"]').addClass('active');
+        $('[data-tab-name="' + tabName + '"][data-tab-body-item-name="' + itemName + '"]').addClass('active');
+      });
+});
+TabBox__init();
+
+$(function initMap(){
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  });
+$(function initMap(){
+    var uluru = {lat: -25.344, lng: 131.036};
+  
+    // The map, centered at Uluru
+  
+    var map = new google.maps.Map(
+  
+        document.getElementById('map'), {zoom: 4, center: uluru});
+  
+    // The marker, positioned at Uluru
+  
+    var marker = new google.maps.Marker({position: uluru, map: map});
 });
--- public/pf/Rolex/index.php
+++ public/pf/Rolex/index.php
@@ -15,10 +15,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
     <!-- 폰트어썸 불러오기 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="../package/swiper-bundle.min.css">
     <!-- 제이쿼리 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="../package/swiper-bundle.min.js"></script>
+    <!-- 구글 맵 -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDL7Ri4QVwConWr55jq1oUo5Ad_1c1L4o&callback=initMap"></script>
     <script src="/pf/Rolex/index.js"></script>
 </head>
 <body>
@@ -80,44 +86,409 @@
     </div>
 </div>
 
-<
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-1.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-2.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-3.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-4.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-5.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-6.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-7.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-8.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-9.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-10.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-11.jpg" alt=""></div>
-             <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-12.jpg" alt=""></div>
-              <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-13.jpg" alt=""></div>
-              <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-14.jpg" alt=""></div>
-            <div class="swiper-slide">
-                <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-15.jpg" alt=""></div>
-        </div>
-
+<div class="rc"><h2>Rolex Collection</h2></div>
+<div class="swiper-container con">
+    <div class="swiper-wrapper con">
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-1.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-2.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-3.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-4.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-5.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-6.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-7.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-8.jpg" alt="">
+        </a></div>
+      <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-9.jpg" alt="">
+        </a></div>
+        <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-10.jpg" alt="">
+        </a></div>
+    <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-11.jpg" alt="">
+        </a></div>
+    <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-12.jpg" alt="">
+        </a></div>
+    <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-13.jpg" alt="">
+        </a></div>
+    <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-14.jpg" alt="">
+        </a></div>
+    <div class="swiper-slide"><a href="#">
+          <img src="https://nohyoochan.github.io/img1/pf/pf1/page3-15.jpg" alt="">
+        </a></div>
+</div>
     <!-- Add Pagination -->
-    <div class="swiper-scrollbar"></div>
+    <div class="swiper-scrollbar con" style="width:50%;"></div>
+    </div>
+
+<div class="set-clock con">
+  <h2>Set Clock</h2>
+</div>
+<div class="box-list-1 con">
+  <div class="flex flex-row-wrap">
+    <div class="box">
+      <div class="content tab-type-1 con">
+        <div class="head">
+          <ul class="flex">
+            <li class="flex-1-0-0 active" data-tab-name="box-1" data-tab-head-item-name="1"><a>클래식 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-1" data-tab-head-item-name="2"><a>프로페셔널 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-1" data-tab-head-item-name="3"><a>남성 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-1" data-tab-head-item-name="4"><a>여성 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-1" data-tab-head-item-name="5"><a>골드 시계</a></li>
+          </ul>
+        </div>
+        <div class="body">
+          <ul>
+            <li class="active" data-tab-name="box-1" data-tab-body-item-name="1">
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-1-7.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-1" data-tab-body-item-name="2">
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-7.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-2-8.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-1" data-tab-body-item-name="3">
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-7.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-3-8.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-1" data-tab-body-item-name="4">
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-4-6.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-1" data-tab-body-item-name="5">
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-7.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/3-5-8.png" alt="">
+                </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     </div>
--
+  </div>
+</div>
+
+<div class="choose-clock con">
+  <h2>Choose Clock</h2>
+</div>
+<div class="box-list-2 con">
+  <div class="flex flex-row-wrap">
+    <div class="box">
+      <div class="content tab-type-2 con">
+        <div class="head">
+          <ul class="flex">
+            <li class="flex-1-0-0 active" data-tab-name="box-2" data-tab-head-item-name="1"><a>남성용 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-2" data-tab-head-item-name="2"><a>여성용 시계</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-2" data-tab-head-item-name="3"><a>오이스터스틸</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-2" data-tab-head-item-name="4"><a>골드 케이스</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-2" data-tab-head-item-name="5"><a>오이스터스틸과 골드</a></li>
+            <li class="flex-1-0-0" data-tab-name="box-2" data-tab-head-item-name="6"><a>보석 세팅 베젤</a></li>
+          </ul>
+        </div>
+        <div class="body">
+          <ul>
+            <li class="active" data-tab-name="box-2" data-tab-body-item-name="1">
+            <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-7.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-1-8.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-2" data-tab-body-item-name="2">
+            <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-1.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-2.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-3.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-4.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-5.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-6.png" alt="">
+                </a>
+                <a href="#">
+                <img src="https://nohyoochan.github.io/img1/pf/pf1/4-2-7.png" alt="">
+                </a>
+            </li>
+            <li data-tab-name="box-2" data-tab-body-item-name="3">입찰공고2 내용</li>
+            <li data-tab-name="box-2" data-tab-body-item-name="4">입찰공고2 내용</li>
+            <li data-tab-name="box-2" data-tab-body-item-name="5">입찰공고2 내용</li>
+            <li data-tab-name="box-2" data-tab-body-item-name="6">입찰공고2 내용</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+<div class="page5">
+  <h2>Learn more about Rolex</h2>
+<div class="main-bar">
+  <div class="img-box con">
+    <img src="https://nohyoochan.github.io/img1/pf/pf1/5-1.png" alt="">
+  </div>
+  <div class="text-box-1">
+    <a href="#">유서 깊은 탁월함
+    <br>롤렉스는 모든 과정에 신중을 기하고 완벽함을 추구합니다.</a>
+    <br>
+    <p>롤렉스의 전문 기술</p>
+    <pre>롤렉스는 시계의 바탕이 되는 전문 기술들을 계속해서 발전시켜 나가며      </pre>
+    <pre>여러 세대를 거쳐 전수해왔습니다.</pre>
+    <pre>전통 기법에서부터 첨단 기술에 이르기까지,</pre>
+    <pre>롤렉스 시계에 담긴 노하우를 자세히 만나보세요.</pre>
+  </div>
+  <div class="img-box">
+    <img src="https://nohyoochan.github.io/img1/pf/pf1/page5-2.jpg" alt="">
+  </div>
+  <div class="text-box-2">
+    <a href="#">롤렉스의 중심이 되는 워치메이커</a>
+    <p>롤렉스의 워치메이커들은 타임피스가 정상적으로
+        <br>작동할수 있도록 세심하게 관리하는 전문가입니다. 
+        <br>초기 설계부터 제조뿐만 아니라 수리와 관리까지
+        <br>모든 공정 단계에 참여합니다.</p>
+  </div>
+    <div class="text-box-2-2">
+      <p>워치 메이킹</p>
+      <pre>워치메이커들은 롤렉스 시계에 완벽함과 끊임없는
+          <br>혁신을 추구하는 워치메이킹 정신을 담아 오래도록
+          <br> 사용할 수 있는 특별한 시계를 완성합니다.</pre>
+    </div>
+  <div class="img-box">
+    <img src="https://nohyoochan.github.io/img1/pf/pf1/page5-3.jpg" alt="">
+  </div>
+  <div class="text-box-3">
+    <a href="#">지구를 지키기 위한 프로젝트</a>
+    <p>영감을 주는 이야기</p>
+  </div>
+</div>
+</div>
+
+<h3>My Google Maps Demo</h3>
+<div id="map"></div>
+    <div class="map-text">
+      <a href="#">공식 판매점 찾기</a>
+    </div>
+  </div>
+</div>
+
+<div class="footer con">
+  <div class="footer-bar">
+    <div class="rolex-collection">
+      <div class="oo">롤렉스 컬렉션</div>
+      <ul>
+        <li><a href="#">Rolex Collection</a></li>
+        <li><a href="#">Set Clock</a></li>
+        <li><a href="#">Choose Rolex</a></li>
+        <li><a href="#">MAN Clock</a></li>
+        <li><a href="#">WOMEN Clock</a></li>
+      </ul>
+    </div>
+    <div class="rolex-world">
+      <div class="oo">롤렉스 세계</div>
+      <ul>
+        <li><a href="#">Learn more about Rolex</a></li>
+      </ul>
+    </div>
+    <div class="service">
+      <div class="oo">서비스</div>
+      <ul>
+        <li><a href="#">공식 판매점 찾기</a></li>
+        <li><a href="#">시계 관리 & 서비스</a></li>
+        <li><a href="#">나의 컬렉션</a></li>
+        <li><a href="#">자주 묻는 질문</a></li>
+      </ul>
+    </div>
+    <div class="collection">
+      <div class="oo">컬렉션</div>
+      <ul>
+        <li><a href="#">AIR-KING</a></li>
+        <li><a href="#">COSMOGRAPH DAYTONA</a></li>
+        <li><a href="#">DATEJUST</a></li>
+        <li><a href="#">LADY-DATEJUST</a></li>
+        <li><a href="#">DAY-DATE</a></li>
+        <li><a href="#">EXPLORER</a></li>
+        <li><a href="#">GMT-Mater||</a></li>
+        <li><a href="#">MILGAUSS</a></li>
+        <li><a href="#">OYSTER PERPETUAL</a></li>
+        <li><a href="#">PEARLMASTER</a></li>
+        <li><a href="#">SEA-DWELLER</a></li>
+        <li><a href="#">SKY-DWELLER</a></li>
+        <li><a href="#">SUBMARINER</a></li>
+        <li><a href="#">YACHT-MASTER</a></li>
+        <li><a href="#">CELLINI</a></li>
+      </ul>
+    </div>
+    <div class="channel">
+      <div class="oo">공식 채널</div>
+      <ul>
+        <li><a href="#">페이스북</a></li>
+        <li><a href="#">인스타그램</a></li>
+        <li><a href="#">트위터</a></li>
+        <li><a href="#">유튜브</a></li>
+        <li><a href="#">핀터레스트</a></li>
+      </ul>
+    </div>
+    <div class="gozi">
+      <div class="oo">법적 고지</div>
+      <ul>
+        <li><a href="#">이용약관</a></li>
+        <li><a href="#">개인 정보 고지</a></li>
+        <li><a href="#">쿠키</a></li>
+      </ul>
+    </div>
+    <div class="media">
+      <div class="oo">미디어</div>
+      <ul>
+        <li><a href="#">바탕화면</a></li>
+        <li><a href="#">브로셔</a></li>
+        <li><a href="#">사용자가이드</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
 </body>
