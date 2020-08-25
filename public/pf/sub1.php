7cb16868621834bb345269f2d6f6f41e330cb1b2
nohyoochan <ssense99@daum.net>
2020-08-26 01:06:18 +0900

    08-26

--- public/pf/sub1.php
+++ public/pf/sub1.php
@@ -16,7 +16,136 @@
 <body>
 <div id="fullpage">
   <div class="section">
-    <div class="main">
+  <div class="SideBar" onclick="SideBar__show();">
+    <img src="https://nohyoochan.github.io/img1/pf/PF/icon.png" alt="">
+</div>
+<div class="side-bar">
+    <div class="side-close" onclick="SideBar__hide();">
+        <img src="https://nohyoochan.github.io/img1/pf/PF/icon-close.png" alt="">
+    </div>
+  <nav class="menu-box-1">
+    <ul>
+      <li>
+        <a href="/pf/index.php" class="block">HOME</a>
+      </li>
+
+      <li>
+        <a href="/pf/sub1.php" class="block">ABOUT ME</a>
+      </li>
+      
+      <li>
+        <a href="/pf/sub2.php" class="block">PORTFOLIO</a>
+      </li>
+
+      <li>
+        <a href="/pf/sub3.php" class="block">EXPERIENCE</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+   <div class="text-1">
+     <p>지금까지 배우고 경험한</p>
+     <p>트랜드에 맞게 최신 디자인을 만들어내며,</p>
+     <p>UI/UX 디자인을 창조합니다.</p> 
+    </div>
+   <div class="text-2">
+     <p>여러 디자인을 통해 더 나은 발상과 아이디어를 계획하고,</p>  
+     <p>클라이언트 요구사항의 다양성, 정확한 목표를 근거로 가치있고 좋은 콘텐츠를 제작하며,</p>
+     <p>인정 받는 디자이너를 꿈꾸며 노력합니다.</p>
+    </div>
+   <div class="text-3">
+     <strong>Communication of empathy and
+     <br>
+     understanding is at the heart of design.</strong>
+   </div>
+   <div class="text-4">
+     <span>It is not a design if you cannot communicate
+     <br> with the client and if there is nothing that people feel when they see it.</span>
+   </div>
+  </div>
+  <div class="section">
+  <div class="main">
+    <div class="about">
+      <h2>ABOUT</h2>
+    </div>
+    <div class="wrap">
+      <div class="img-bar">
+        <a href="#">
+          <img src="" alt="">
+        </a>
+      </div>
+      <div class="text-5">
+        <div class="edu">
+          <p>
+            <span>EDUCATION</span>
+          </p>
+        </div>
+        <div class="edu-text">
+          <p>
+            <span>2020. 04 - 2020. 10</span>
+            <span>웹디자인(UI/UX 반응형 웹) [ SBS 아카데미 ]</span>
+          </p>
+        </div>
+        <div class="license">
+          <p>
+            <span>LICENSE</span>
+          </p>
+        </div>
+        <div class="license-text">
+          <p>
+            <span>2020. 04 - 2020. 05</span>
+            <span>웹디자인 기능사 필기 취득</span>
+          </p>
+        </div>
+        <div class="prf">
+        <p>
+          <span>노유찬</span>
+        </p>
+        <p>010 - 6385 - 2756</p>
+        <p>ssense99@daum.net</p>
+        </div>
+      </div>
+      <div class="pro-line">
+      <div class="ht">
+        <p>
+          <span>책임감과 성실성</span>
+        </p>
+      </div>
+      <div class="graph stack">
+        <span>90%</span>
+      </div>
+      <div class="cs">
+        <p>
+          <span>열정</span>
+        </p>
+      </div>
+      <div class="graph2 stack2">
+        <span>95%</span>
+      </div>
+      <div class="ph">
+        <p>
+          <span>도전정신</span>
+        </p>
+      </div>
+      <div class="graph3 stack3">
+        <span>95%</span>
+      </div>
+      <div class="il">
+        <p>
+          <span>전문역량</span>
+        </p>
+      </div>
+      <div class="graph4 stack4">
+        <span>80%</span>
+      </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+  <div class="section">
+  <div class="slide">
+  <div class="main">
       <div class="title">
       <strong>DESIGN SKILL</strong>
       </div>
@@ -61,9 +190,6 @@
       </div>
     </div>
   </div>
-  <div class="section">
-  <div class="slide">
-  <div class="slide">
   </div>
   <div class="slide">3</div>
   </div>
